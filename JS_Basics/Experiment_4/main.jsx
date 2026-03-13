337c15b2ae85251eb3ab44d36481d845c7d53dac
Parth Agarwal <parthagl2006@gmail.com>
2026-03-13 10:30:11 +0530

    Experiments

---
+++ JS_Basics/Experiment_4/main.jsx
@@ -0,0 +1,42 @@
+function calculateResult()
+{
+    let n = document.getElementById("subjects").value;
+    let total = 0;
+    for(let i = 1; i <= n; i++)
+    {
+        let marks = parseFloat(prompt("Eneter the marks for Subject " + i));
+        total = total + marks;
+    }
+    let average = total / n;
+    let grade;
+    let result;
+    if(average >= 90)
+    {
+        grade = "A+";
+    }
+    else if(average >= 75)
+    {
+        grade = "A";
+    }
+    else if(average >= 60)
+    {
+        grade = "B";
+    }
+    else if(average >= 40)
+    {
+        grade = "C";
+    }
+    else
+    {
+        grade = "F";
+    }
+    if(grade == "F")
+    {
+        result = "Fail";
+    }
+    else
+    {
+        result = "Pass";
+    }
+    document.getElementById("Total Marks : ") + total + (<br>) + ("Average : ") + average + (<br>) + ("Grade : ") + grade + (<br>) + ("Result : ") + (result);
+}
--- JS_Basics/xyz.js
+++ JS_Basics/xyz.js
@@ -4,7 +4,18 @@ console.log(name);*/
 /*let a = 10;
 a = "Parth";
 console.log(a);*/
-{
+/*{
 const name = 20;
 console.log(typeof name);
-}
+}*/
+const add = (a, b) => a + b;
+console.log(add(5, 2));
+let numbers = [1, 2, 3, 4, 5, 6, 7];
+let square = numbers.map(num => num * num);
+console.log(square);
+let numbers2 = [10, 15, 20, 25];
+let result = numbers2.filter(num => num > 15);
+console.log(result);
+let numbers3 = [20, 40, 60, 80, 100];
+let sum = numbers3.reduce((total, num) => total + num, 0);
+console.log(sum);
